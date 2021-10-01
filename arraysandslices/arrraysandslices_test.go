0a22779bc79502d2bad8c3a39418f8ffc5873016
Kevin Mathew S <kevin.mathew@mindgeekconsultants.com>
2021-10-01 17:40:36 +0530

    Testing sum on slices

--- arraysandslices/arrraysandslices_test.go
+++ arraysandslices/arrraysandslices_test.go
@@ -6,14 +6,27 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	numbers := []int{1, 2, 3, 4, 5, 6}
-	got := Sum(numbers)
-	want := 21
 
-	if got != want {
-		var gotString string = fmt.Sprint(got)
-		var wantString string = fmt.Sprint(want)
-		errormsg := "Expecting " + wantString + " and recieved " + gotString
-		t.Errorf(errormsg)
+	assertTest := func(t testing.TB, got, want int) {
+		if got != want {
+			var gotString string = fmt.Sprint(got)
+			var wantString string = fmt.Sprint(want)
+			errormsg := "Expecting " + wantString + " and recieved " + gotString
+			t.Errorf(errormsg)
+		}
 	}
+	t.Run("Collection of 6", func(t *testing.T) {
+
+		numbers := []int{1, 2, 3, 4, 5, 6}
+		got := Sum(numbers)
+		want := 21
+		assertTest(t, got, want)
+	})
+	t.Run("Collection of 5", func(t *testing.T) {
+
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		assertTest(t, got, want)
+	})
 }
