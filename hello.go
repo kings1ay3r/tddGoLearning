7e76310444c732e5c6bc8483e2cd63168c0ec78e
Kevin Mathew S <kevin.mathew@mindgeekconsultants.com>
2021-10-01 16:42:06 +0530

    Checking with if for french also

--- hello.go
+++ hello.go
@@ -2,15 +2,21 @@ package main
 
 import "fmt"
 
-const helloPrefix = "Hello, "
-
-func Hello(name string) string {
+func Hello(name string, language string) string {
+	var helloPrefix string
 	if name == "" {
 		name = "World"
+	}
+	if language == "es" {
+		helloPrefix = "Hola, "
+	} else if language == "fr" {
+		helloPrefix = "Bonjour, "
+	} else {
+		helloPrefix = "Hello, "
 	}
 	return helloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Kevin"))
+	fmt.Println(Hello("Kevin", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -12,14 +12,27 @@ func TestHello(t *testing.T) {
 		}
 	}
 
-	t.Run("Saying Hello to somone", func(t *testing.T) {
-		got := Hello("Kevin")
-		want := "Hello, Kevin"
+	t.Run("Saying Hello to somone in Spanish", func(t *testing.T) {
+		got := Hello("Eragon", "es")
+		want := "Hola, Eragon"
+		testAssertion(t, got, want)
+
+	})
+
+	t.Run("Saying Hello to somone in French", func(t *testing.T) {
+		got := Hello("Arya", "fr")
+		want := "Bonjour, Arya"
 		testAssertion(t, got, want)
 
 	})
 	t.Run("Saying Hello World When empty string provided", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
+		want := "Hello, World"
+		testAssertion(t, got, want)
+
+	})
+	t.Run("Saying Hello World When empty string provided", func(t *testing.T) {
+		got := Hello("", "")
 		want := "Hello, World"
 		testAssertion(t, got, want)
 
