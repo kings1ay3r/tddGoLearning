8582bacedb5d3fac8be1c9526e98cbc5281c2ea6
Kevin Mathew S <kevin.mathew@mindgeekconsultants.com>
2021-10-01 16:30:33 +0530

    Refactored first test

--- hello.go
+++ hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const helloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return helloPrefix + name
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Kevin")
-	want := "Hello, Kevin"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+
+	testAssertion := func(t testing.TB, got, want string) {
+		t.Helper() //! TODO tounderstand
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Saying Hello to somone", func(t *testing.T) {
+		got := Hello("Kevin")
+		want := "Hello, Kevin"
+		testAssertion(t, got, want)
+
+	})
+	t.Run("Saying Hello World When empty string provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		testAssertion(t, got, want)
+
+	})
 }
