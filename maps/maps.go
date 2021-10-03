8ac6697758579817acf1748fbd3b58a8cc56b3c2
Kevin Mathew S <kevin.mathew@mindgeekconsultants.com>
2021-10-03 22:54:10 +0530

    Dictionary API with Maps Completed

--- maps/maps.go
+++ maps/maps.go
@@ -5,6 +5,7 @@ import "errors"
 type Haystack map[string]string
 
 var ErrNOtFound = errors.New("invalid word")
+var ErrWordExist = errors.New("Word Exists")
 
 func (h Haystack) search(needle string) (string, error) {
 	defenition, ok := h[needle]
@@ -12,4 +13,34 @@ func (h Haystack) search(needle string) (string, error) {
 		return defenition, ErrNOtFound
 	}
 	return defenition, nil
+}
+
+func (h Haystack) Add(key, value string) error {
+	_, ok := h[key]
+	if ok {
+		return ErrWordExist
+	}
+	h[key] = value
+	return nil
+}
+
+func (h Haystack) Update(key, value string) error {
+	_, ok := h[key]
+	if ok {
+		h[key] = value
+		return nil
+	} else {
+		return ErrNOtFound
+
+	}
+}
+
+func (h Haystack) Delete(key string) error {
+	_, ok := h[key]
+	if ok {
+		delete(h, key)
+		return nil
+	} else {
+		return ErrNOtFound
+	}
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -14,17 +14,57 @@ func TestSearch(t *testing.T) {
 		}
 	}
 
-	t.Run("Testing Known Word", func(t *testing.T) {
+	t.Run("Read Known Word", func(t *testing.T) {
 		dictionary := Haystack{"test": "defenition of test"}
 		got, _ := dictionary.search("test")
 		want := "defenition of test"
 
 		testAssertion(t, got, want)
 	})
-
-	t.Run("Testing Known Word", func(t *testing.T) {
+	t.Run("Read UnKnown Word", func(t *testing.T) {
 		dictionary := Haystack{"test": "defenition of test"}
 		_, got := dictionary.search("retest")
+		want := ErrNOtFound
+		testAssertion(t, got.Error(), want.Error())
+	})
+	t.Run("Create New Word", func(t *testing.T) {
+		dictionary := Haystack{}
+		dictionary.Add("test", "defenition of test")
+		got, _ := dictionary.search("test")
+		want := "defenition of test"
+
+		testAssertion(t, got, want)
+	})
+	t.Run("Create New Word, Exist Error", func(t *testing.T) {
+		dictionary := Haystack{"test": "defenition of test"}
+		got := dictionary.Add("test", "defeniton of test")
+		want := ErrWordExist
+
+		testAssertion(t, got.Error(), want.Error())
+	})
+	t.Run("Updating Word", func(t *testing.T) {
+		dictionary := Haystack{"test": "defenition of test"}
+		dictionary.Update("test", "test Redefined")
+		got, _ := dictionary.search("test")
+		want := "test Redefined"
+		testAssertion(t, got, want)
+	})
+	t.Run("Updating Word : Exist Error", func(t *testing.T) {
+		dictionary := Haystack{"test": "defenition of test"}
+		got := dictionary.Update("retest", "meaning of retest")
+		want := ErrNOtFound
+		testAssertion(t, got.Error(), want.Error())
+	})
+	t.Run("Delete Word", func(t *testing.T) {
+		dictionary := Haystack{"test": "defenition of test"}
+		dictionary.Delete("test")
+		_, got := dictionary.search("test")
+		want := ErrNOtFound
+		testAssertion(t, got.Error(), want.Error())
+	})
+	t.Run("Delete Word : Unknowm Error", func(t *testing.T) {
+		dictionary := Haystack{}
+		got := dictionary.Delete("test")
 		want := ErrNOtFound
 		testAssertion(t, got.Error(), want.Error())
 	})
